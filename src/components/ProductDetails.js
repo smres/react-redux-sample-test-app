f59e43220db56e35dfe3c201d5d5e53890f4338d
Mohammad Reza esmailzadeh <smr8012101067@gmail.com>
2023-08-06 23:27:21 +0330

    ddd

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,46 +1,63 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { add } from "../stateManagement/actions/productActions";
 
 const ProductDetails = () => {
-    const itemsState = useSelector((state) => (state.productStateInitial));
-    const dispatch = useDispatch();
+  const itemsState = useSelector((state) => state.productStateInitial);
+  const dispatch = useDispatch();
 
-    console.log("sss = ", itemsState);
+  console.log("sss = ", itemsState);
 
-    const save = (event) => {
-        event.preventDefault();
-        const form = new FormData(event.target);
-        const id = form.get("productId");
-        const productName = form.get("productName");
-        const price = form.get("price");
+  const save = (event) => {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const id = form.get("productId");
+    const productName = form.get("productName");
+    const price = form.get("price");
 
-        dispatch(add({ id, productName, price }))
-        alert("Add item Succ");
+    dispatch(add({ id, productName, price }));
+    alert("Add item Success...");
 
-        // clear form after Submit
-        event.target.reset();
-    }
-    return (
-        <>
-            <h1>Add New Product</h1>
-            <form onSubmit={(event) => save(event)}>
-                <div class="form-group">
-                    <label>Product ID:</label>
-                    <input type="number" class="form-control" name='productId' />
-                </div>
-                <div class="form-group">
-                    <label>Product Name:</label>
-                    <input type="text" class="form-control" name='productName' />
-                </div>
-                <div class="form-group">
-                    <label>Price:</label>
-                    <input type="number" class="form-control" name='price' />
-                </div>
-                <button type="submit" class="btn btn-primary">Save</button>
-            </form>
-        </>
-    );
+    // clear form after Submit
+    event.target.reset();
+  };
+  return (
+    <>
+      <h1>Add New Product</h1>
+      <form onSubmit={save}>
+        <div className="form-group">
+          <label htmlFor="productId">Product ID:</label>
+          <input
+            id="productId"
+            type="number"
+            className="form-control"
+            name="productId"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="productName">Product Name:</label>
+          <input
+            id="productName"
+            type="text"
+            className="form-control"
+            name="productName"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">Price:</label>
+          <input
+            id="price"
+            type="number"
+            className="form-control"
+            name="price"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Save
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default ProductDetails;
--- src/stateManagement/reducers/productReducer.js
+++ src/stateManagement/reducers/productReducer.js
@@ -1,44 +1,49 @@
 // import { connect } from "react-redux";
-import { productGetAll, productGetById, productAdd, productRemove } from "../actions/actionTypes"
+import {
+  productGetAll,
+  productGetById,
+  productAdd,
+  productRemove,
+} from "../actions/actionTypes";
 
 const initialState = {
-    items: [
-        { id: 1, productName: "Product A", price: 100 },
-        { id: 2, productName: "Product B", price: 200 },
-        { id: 3, productName: "Product C", price: 300 },
-    ],
-    isLoading: false
-}
+  items: [
+    { id: 1, productName: "Product A", price: 100 },
+    { id: 2, productName: "Product B", price: 200 },
+    { id: 3, productName: "Product C", price: 300 },
+  ],
+  isLoading: false,
+};
 
 function productReducer(state = initialState, action) {
-    switch (action.type) {
-        case productGetAll:
-            return {
-                ...state,
-                items: state.items,
-            };
+  switch (action.type) {
+    case productGetAll:
+      return {
+        ...state,
+        items: state.items,
+      };
 
-        case productGetById:
-            return {
-                ...state,
-                items: state.items.find((q) => q.id === action.payload),
-            };
+    case productGetById:
+      return {
+        ...state,
+        items: state.items.find((q) => q.id === action.payload),
+      };
 
-        case productAdd:
-            return {
-                ...state,
-                items: state.items.concat([action.payload]),
-            };
+    case productAdd:
+      return {
+        ...state,
+        items: state.items.concat([action.payload]),
+      };
 
-        case productRemove:
-            return {
-                ...state,
-                items: state.items.filter((q) => q.id !== action.payload),
-            };
+    case productRemove:
+      return {
+        ...state,
+        items: state.items.filter((q) => q.id !== action.payload),
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }
 
 export default productReducer;
